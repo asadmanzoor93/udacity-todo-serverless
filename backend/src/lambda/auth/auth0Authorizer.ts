3dc8005a7611b2f9ad5b3cdb79e5a56bcf18f621
Asad Manzoor <asad33manzoor@gmail.com>
2021-10-03 02:08:11 +0500

    Oauth2 403 permission issue fixed

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,12 +1,14 @@
 import {CustomAuthorizerEvent, CustomAuthorizerResult} from 'aws-lambda'
 import 'source-map-support/register'
 
-import {verify} from 'jsonwebtoken'
+import {decode, verify} from 'jsonwebtoken'
 import {createLogger} from '../../utils/logger'
+import Axios from 'axios'
+import {Jwt} from '../../auth/Jwt'
 import {JwtPayload} from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-const AUTH_0_SECRET = process.env.AUTH_0_SECRET
+const jwksUrl = 'https://dev-ucm--6lu.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -50,8 +52,18 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  logger.info('Authorizing TOKEN: '+ token + ' :: AUTH_0_SECRET: ' + AUTH_0_SECRET)
-  return verify(token, AUTH_0_SECRET) as JwtPayload
+  logger.info('Authorizing TOKEN: '+ token)
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  const response = await Axios.get(jwksUrl);
+  // @ts-ignore
+  const signingKey = response.data.keys.find(key => key.kid === jwt.header.kid);
+  let certValue:string = signingKey.x5c[0];
+
+  certValue = certValue.match(/.{1,64}/g).join('\n');
+  const finalCertKey:string = `-----BEGIN CERTIFICATE-----\n${certValue}\n-----END CERTIFICATE-----\n`;
+
+ return verify(token, finalCertKey, {algorithms: ['RS256']}) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
